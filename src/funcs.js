106e24cd24a67a91887cd4be0ae33c3abc3f9b07
Roman <darkridder.pro@gmail.com>
2019-01-24 21:45:28 +0800

    add various functions for simple actions

---
+++ src/funcs.js
@@ -0,0 +1,13 @@
+export const isEmpty = function(value){
+    return (value == null || value.length === 0);
+};
+
+export const addClass = function(elem, name){
+    elem.classList.add(name);
+};
+export const hasClass = function(elem, name){
+    return elem.classList.contains(name);
+};
+export const removeClass = function(elem, name){
+    elem.classList.remove(name);
+};
